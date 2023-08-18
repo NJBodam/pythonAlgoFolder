12070bf536fbf9d2cc70bd0f80a1b16f24b3e125
Jerry Bodam <90649783+NJBodam@users.noreply.github.com>
2023-08-18 17:40:24 +0200

    Standardsing and Groupby
    How to standardise on column or row based levels and how to implement groupby function.

---
+++ n10_groupby.py
@@ -0,0 +1,97 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+values = np.array([1, 3, 2, 4, 1, 6, 4])
+example_df = pd.DataFrame({
+    'value': values,
+    'even': values % 2 == 0,
+    'above_three': values > 3 
+}, index=['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+
+# Change False to True for each block of code to see what it does
+
+# Examine DataFrame
+if False:
+    print example_df
+
+# Out[1]:
+#    value   even  above_three
+# a      1  False        False
+# b      3  False        False
+# c      2   True        False
+# d      4   True         True
+# e      1  False        False
+# f      6   True         True
+# g      4   True         True
+
+
+# Examine groups
+if False:
+    grouped_data = example_df.groupby('even')   # groupby() returns a GroupBy object where indexes are grouped according to the values of the column 'even'
+    # The groups attribute is a dictionary mapping keys to lists of row indexes
+    print grouped_data.groups   
+
+# Out[2]:
+# {False: ['a', 'b', 'e'], True: ['c', 'd', 'f', 'g']}      # keys are the values of the column 'even' and the values are the indexes of the rows that have that value
+
+# Group by multiple columns
+if False:
+    grouped_data = example_df.groupby(['even', 'above_three'])
+    print grouped_data.groups
+
+# Out[3]:
+# {(False, False): ['a', 'b', 'e'], (True, False): ['c'], (True, True): ['d', 'f', 'g']}
+
+# Get sum of each group
+if False:
+    grouped_data = example_df.groupby('even')
+    print grouped_data.sum()
+
+# Out[4]:
+#          value  above_three
+# even                     
+# False      5            0     # 5 = sum of the values of the rows that have 'False' in the column 'even': 0 = sum of the False values in the column 'even' that are above 3
+# True      16            3     # 16 = sum of the values of the rows that have 'True' in the column 'even': 3 = sum of the True values in the column 'even' that are above 3
+
+
+# Limit columns in result
+if False:
+    grouped_data = example_df.groupby('even')
+    
+    # You can take one or more columns from the result DataFrame
+    print grouped_data.sum()['value']
+    
+    print '\n' # Blank line to separate results
+    
+    # You can also take a subset of columns from the grouped data before 
+    # collapsing to a DataFrame. In this case, the result is the same.
+    print grouped_data['value'].sum()
+
+# Out[5]:
+# even
+# False     5
+# True     16
+# Name: value, dtype: int64
+#
+# even
+# False     5
+# True     16
+    
+filename = '/datasets/ud170/subway/nyc_subway_weather.csv'
+subway_df = pd.read_csv(filename)
+
+### Write code here to group the subway data by a variable of your choice, then
+### either print out the mean ridership within each group or create a plot.
+
+# For setting the index to a column
+# grp_data = subway_df.set_index('UNIT')
+
+
+if True:
+    grouped_data = subway_df[:10].groupby('hour')
+    
+    gdt = subway_df[:10].groupby('day_week')
+#     print(grouped_data.groups)
+    print(gdt.sum())
--- n9_standardizing_vectorized_ops.py
+++ n9_standardizing_vectorized_ops.py
@@ -15,7 +15,18 @@ if False:
     print df + s
 
     #Output:
-    #     0   1   2   3
+#     0   1    2    3
+# 0  10  50   90  130
+# 1  20  60  100  140
+# 2  30  70  110  150
+# 3  40  80  120  160
+
+#     0   1    2    3
+# 0  11  52   93  134
+# 1  21  62  103  144
+# 2  31  72  113  154
+# 3  41  82  123  164
+
     
 # Adding with axis='index'
 if False:
@@ -31,6 +42,19 @@ if False:
     print '' # Create a blank line between outputs
     print df.add(s, axis='index')
     # The functions sub(), mul(), and div() work similarly to add()
+
+   #Output:
+#     0   1    2    3
+# 0  10  50   90  130
+# 1  20  60  100  140
+# 2  30  70  110  150
+# 3  40  80  120  160
+
+#     0   1    2    3
+# 0  11  51   91  131
+# 1  22  62  102  142
+# 2  33  73  113  153
+# 3  44  84  124  164
     
 # Adding with axis='columns'
 if False:
@@ -46,7 +70,19 @@ if False:
     print '' # Create a blank line between outputs
     print df.add(s, axis='columns')
     # The functions sub(), mul(), and div() work similarly to add()
-    
+
+    #Output:
+    # 0   1    2    3
+# 0  10  50   90  130
+# 1  20  60  100  140
+# 2  30  70  110  150
+# 3  40  80  120  160
+
+#     0   1    2    3
+# 0  11  52   93  134
+# 1  21  62  103  144
+# 2  31  72  113  154
+# 3  41  82  123  164
 grades_df = pd.DataFrame(
     data={'exam1': [43, 81, 78, 75, 89, 70, 91, 65, 98, 87],
           'exam2': [24, 63, 56, 56, 67, 51, 79, 46, 72, 60]},
@@ -63,7 +99,10 @@ def standardize(df):
     This time, try to use vectorized operations instead of apply().
     You should get the same results as you did before.
     '''
-    return None
+
+    mean = df.mean(axis='index')
+    std = df.std(axis='index', ddof=0)
+    return (df.sub(mean)).div(std)
 
 def standardize_rows(df):
     '''
@@ -72,4 +111,35 @@ def standardize_rows(df):
     
     This one is more challenging than standardizing each column!
     '''
-    return None
+
+    mean = df.mean(axis='columns')
+    std = df.std(axis='columns', ddof=0)
+    return (df.sub(mean, axis='index')).div(std, axis='index')  # this takes the mean of each row and subtracts it from each element in the row, then divides by the standard deviation of each row
+
+print(standardize(grades_df))
+print(standardize_rows(grades_df))
+
+#Output:
+#           exam1     exam2
+# Andre   -2.315341 -2.304599
+# Barry    0.220191  0.386400
+# Chris    0.020017 -0.096600
+# Dan     -0.180156 -0.096600
+# Emilio   0.753987  0.662400
+# Fred    -0.513779 -0.441600
+# Greta    0.887436  1.490400
+# Humbert -0.847401 -0.786600
+# Ivan     1.354508  1.007400
+# James    0.620538  0.179400
+
+#           exam1     exam2
+# Andre      1.0   -1.0
+# Barry      1.0   -1.0
+# Chris      1.0   -1.0
+# Dan        1.0   -1.0
+# Emilio     1.0   -1.0
+# Fred       1.0   -1.0
+# Greta      1.0   -1.0
+# Humbert    1.0   -1.0
+# Ivan       1.0   -1.0
+# James      1.0   -1.0
